467c277238488f7078ac62abcc1047d4d8b3f54a
Haley Someone <hbarlar1@gmail.com>
2018-07-23 23:08:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,7 @@ def prime?(number)
   (1...number).each do |i|
     if number % i == 0
       return true
-    elsif number < 1
-      return false
-    else
-      return false
 end
+return false
 end
 end
